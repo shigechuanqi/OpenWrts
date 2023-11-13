01d7911cc140d97c5da8c737657ea66a4feaae06
lengxugz <lengxugz@gmail.com>
2023-11-13 12:42:39 +0800

    update

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Modify default system settings
 
-# 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+# 修改默认IP为192.168.100.1
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
@@ -12,5 +12,5 @@ echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-pa
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
 # 替换默认主题
-rm -rf package/lean/luci-theme-argon 
+rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
--- package.sh
+++ package.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
 git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
-# vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
 
 # Theme
 # luci-theme-neobird
