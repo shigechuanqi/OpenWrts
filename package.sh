f3e00f9140e142c6faf73bbbe37350ca14f33c28
lengxugz <lengxugz@gmail.com>
2023-11-16 17:14:04 +0800

    add eqosplus

--- package.sh
+++ package.sh
@@ -4,6 +4,9 @@ git clone  https://github.com/lengxugz/OpenwrtApp package/otherapp/OpenwrtApp
 #OpenAppFliter
 #git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 
+#eQosPlus
+git clone https://github.com/sirpdboy/luci-app-eqosplus package/otherapp/luci-app-eqosplus
+
 # Theme
 # luci-theme-neobird
 git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
